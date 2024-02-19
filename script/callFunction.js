48b064504eb74a7d2eaeb8dee5477b1a3c2af9fe
AntorRoyTOxi <antorroyanu558@gmail.com>
2024-02-19 18:49:41 +0600

    completed assginment 5

--- script/callFunction.js
+++ script/callFunction.js
@@ -1,52 +1,39 @@
- function colorAdd(elementId){ 
-      const element=document.getElementById(elementId); 
-        element.classList.add('bg-[#1DD100]')
- };  
-  function showLastBar(elementId){ 
-      const showBar=document.getElementById(elementId); 
-      showBar.classList.remove('hidden');
-  };
-   function hideBar(elementId){ 
-      const hideElement=document.getElementById(elementId); 
-       hideElement.classList.add('hidden');
-   };  
-      function getInnerText(id,value){ 
-         document.getElementById(id).innerText=value;
-      }; 
-        
+function colorAdd(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-[#1DD100]");
+}
+function showLastBar(elementId) {
+  const showBar = document.getElementById(elementId);
+  showBar.classList.remove("hidden");
+}
+function hideBar(elementId) {
+  const hideElement = document.getElementById(elementId);
+  hideElement.classList.add("hidden");
+}
+function getInnerText(id, value) {
+  document.getElementById(id).innerText = value;
+}
 
+function totalCost(id, value) {
+  const totalPrize = document.getElementById(id).innerText;
+  const prize = parseInt(totalPrize);
+  const totalAmount = prize + value;
 
-    //    total cost ---------------------
-        function totalCost(id,value){ 
-             const  totalPrize=document.getElementById(id).innerText;   
-               const prize=parseInt(totalPrize); 
-                 const totalAmount=prize+value; 
-                //    totalPrize.innerText=totalAmount; 
-                getInnerText("total-cost", totalAmount); 
-     
-              
-        }; 
-          
-        // ----------- grand total cost  
-          
-         
-        function grandTotalCost(id,value){ 
-            const  totalPrize=document.getElementById(id).innerText;   
-              const prize=parseInt(totalPrize); 
-                const totalAmount=prize+value; 
-               //    totalPrize.innerText=totalAmount; 
-               getInnerText("grand-total", totalAmount); 
-        };
+  getInnerText("total-cost", totalAmount);
+}
 
+function grandTotalCost(id, value) {
+  const totalPrize = document.getElementById(id).innerText;
+  const prize = parseInt(totalPrize);
+  const totalAmount = prize + value;
 
-        function totalCostGrand(id,value){ 
-            const  totalPrize=document.getElementById(id).innerText;   
-              const prize=parseInt(totalPrize); 
-                const totalAmount=prize+value; 
-               //    totalPrize.innerText=totalAmount; 
-               console.log(totalAmount)
-    
-             
-       }; 
-    
-  
+  getInnerText("grand-total", totalAmount);
+}
+
+function totalCostGrand(id, value) {
+  const totalPrize = document.getElementById(id).innerText;
+  const prize = parseInt(totalPrize);
+  const totalAmount = prize + value;
+
+  console.log(totalAmount);
+}
